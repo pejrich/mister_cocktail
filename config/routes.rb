7dd1b06bd651f265b5bf079340e41cbc9b6244c4
Peter Richards <peter.jrichards@yahoo.com>
2017-08-10 18:20:05 +0800

    end livecode

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
